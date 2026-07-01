1914efdf2939361fcce8b1736c7c3b2caa7acf9b
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-01 21:16:05 +0100

    Only chech ka status code and implement its like ok is 200,created is 201 etc

--- src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.Practices.Crud_SpringBoot_Application.Controller;
 
 import com.Practices.Crud_SpringBoot_Application.Entity.student;
 import com.Practices.Crud_SpringBoot_Application.Services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +16,11 @@ public class StudentController {
     }
 
     @PostMapping("/create")
-    public student createStudent(@RequestBody student student){
+    public ResponseEntity<student> createStudent(@RequestBody student student){
         System.out.println("inside StudentController createStudent");
        student createdStudent= studentService.createStudent(student);
         System.out.println("Exiting StudentController createStudent");
-        return createdStudent;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
     }
 
 }
