60a29c7376974f295798907e926c3bf041bcd84f
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-01 20:10:50 +0100

    create a method of post request in controller class which handle the poststudent data

--- src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
@@ -1,4 +1,17 @@
 package com.Practices.Crud_SpringBoot_Application.Controller;
 
+import com.Practices.Crud_SpringBoot_Application.Entity.student;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
 public class StudentController {
+
+    @PostMapping("/create")
+    public String createStudent(@RequestBody student student){
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return "student created";
+    }
+
 }
--- src/main/java/com/Practices/Crud_SpringBoot_Application/CrudSpringBootApplication.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/CrudSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.Practices.Crud_SpringBoot_Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class CrudSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringBootApplication.class, args);
+        System.out.println("heloo byeio");
 	}
 
 }
