177f79550cdbd7d822ecf12ac9aa327a48441469
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-01 20:54:30 +0100

    write bussines logic in service class to perfome some feature on student

--- src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Controller/StudentController.java
@@ -1,17 +1,21 @@
 package com.Practices.Crud_SpringBoot_Application.Controller;
 
 import com.Practices.Crud_SpringBoot_Application.Entity.student;
+import com.Practices.Crud_SpringBoot_Application.Services.StudentService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @PostMapping("/create")
-    public String createStudent(@RequestBody student student){
-        System.out.println(student.getFirstName());
-        System.out.println(student.getLastName());
-        return "student created";
+    public void createStudent(@RequestBody student student){
+       student createdStudent= studentService.createStudent(student);
     }
 
 }
--- src/main/java/com/Practices/Crud_SpringBoot_Application/Services/StudentService.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Services/StudentService.java
@@ -1,4 +1,21 @@
 package com.Practices.Crud_SpringBoot_Application.Services;
 
+import com.Practices.Crud_SpringBoot_Application.Entity.student;
+import com.Practices.Crud_SpringBoot_Application.Repository.StudentRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public student createStudent(student studentRequest) {
+       student studentresponse=
+               studentRepository.saveStudent(studentRequest);
+        // Implementation for creating a student
+        return studentresponse;
+    }
 }
