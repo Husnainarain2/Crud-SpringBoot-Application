26823ef8d026b5fa0105ab5344d8bdd474e94438
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-11 13:32:28 +0100

    Create DTO classes to simplifiy our class and write in professional way

---
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Dto/CreateStudentResquestDto.java
@@ -0,0 +1,49 @@
+package com.Practices.Crud_SpringBoot_Application.Dto;
+
+public class CreateStudentResquestDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private String gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
