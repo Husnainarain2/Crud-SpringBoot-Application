76e467678446b412f15d9df4195d3a5bc8d25f46
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-12 14:07:31 +0100

    create a Recption Handler dto class to show the response after exceptiom

--- src/main/java/com/Practices/Crud_SpringBoot_Application/Dto/ExceptionResponseDto.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Dto/ExceptionResponseDto.java
@@ -1,5 +1,52 @@
 package com.Practices.Crud_SpringBoot_Application.Dto;
 
-public class ExceptionResponseDto {
+import java.time.LocalDateTime;
 
+public class ExceptionResponseDto {
+    private LocalDateTime timestamp;
+    private int statusCode;
+    private String error;
+    private String message;
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    private String path;
 }
