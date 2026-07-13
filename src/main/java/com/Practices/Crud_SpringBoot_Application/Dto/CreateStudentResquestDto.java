deaabd869594773000169e9ea2f92970a1abd8b3
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-13 13:51:51 +0100

    Set the validation

--- src/main/java/com/Practices/Crud_SpringBoot_Application/Dto/CreateStudentResquestDto.java
+++ src/main/java/com/Practices/Crud_SpringBoot_Application/Dto/CreateStudentResquestDto.java
@@ -1,14 +1,23 @@
 package com.Practices.Crud_SpringBoot_Application.Dto;
 
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.*;
 
 public class CreateStudentResquestDto {
 
     @NotBlank(message = "Name cannot be null/Empty or blank")
+    @Size(min = 2, max = 50, message = "Student name must be within 2 to 50 character long")
     private String firstName;
     private String lastName;
-    private String email;
+
+    @NotNull(message = "Age is required")
+    @Min(value = 18, message = "Student must be atleast 18 years old")
     private int age;
+
+    @NotBlank(message = "Student email cannot be blank")
+    @Email(message = "Student email must be valid")
+    private String email;
+    @NotNull(message = "Gender details  is " +
+            "required")
     private String gender;
 
     public String getFirstName() {
